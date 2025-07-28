b65abc9e1095f2dfe48ff6c6d53738844cff4c01
Abigiya <ajorken1502@gmail.com>
2025-07-28 18:55:02 +0300

    feat(room-service): setup RoomService structure and fields

--- src/main/java/service/RoomService.java
+++ src/main/java/service/RoomService.java
@@ -1,1 +1,18 @@
-// RoomService.java
+package service;
+
+import dao.RoomDAO;
+import exception.ServiceException;
+import model.Room;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RoomService {
+    private final RoomDAO roomDAO;
+    private final Map<Integer, Room> cache = new HashMap<>();
+
+    public RoomService(RoomDAO roomDAO) {
+        this.roomDAO = roomDAO;
+    }
+}
